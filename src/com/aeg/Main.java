d56107f61c0f9c3c456ada540e4b78fcee0fa218
samvrai <4573rix>
2017-01-16 16:12:44 +0100

    Project Refactor.

--- src/com/aeg/Main.java
+++ src/com/aeg/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("");
         System.out.println("Renombrando ficheros en " + args[0]);
         System.out.println("Seguro? s/n");
         Scanner scanner = new Scanner(System.in);
